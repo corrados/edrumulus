9f64e47bd4aa35200bdb068cc60d4800ffba29c4
Volker Fischer <corrados@users.noreply.github.com>
2020-12-22 23:10:54 +0100

    bluetooth MIDI testing (BLE-MIDI to iPad testing -> works)

--- edrumulus.ino
+++ edrumulus.ino
@@ -26,8 +26,11 @@
 #include "edrumulus.h"
 
 #ifdef USE_MIDI
-#include <MIDI.h>
-MIDI_CREATE_DEFAULT_INSTANCE();
+#include <MIDI.h>                   // Hairless USB MIDI
+MIDI_CREATE_DEFAULT_INSTANCE();     // Hairless USB MIDI
+//#include <BLEMIDI_Transport.h>      // BLE MIDI
+//#include <hardware/BLEMIDI_ESP32.h> // BLE MIDI
+//BLEMIDI_CREATE_DEFAULT_INSTANCE();  // BLE MIDI
 #endif
 
 Edrumulus edrumulus;
