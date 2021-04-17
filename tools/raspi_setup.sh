796378b8141f2de05b8c62b97d3a909343330c88
Volker Fischer <corrados@users.noreply.github.com>
2021-04-17 18:50:39 +0200

    jackd2 was missing

--- tools/raspi_setup.sh
+++ tools/raspi_setup.sh
@@ -4,7 +4,7 @@ echo "This script prepares a Raspberry Pi for Edrumulus usage"
 
 
 # install required packages ----------------------------------------------------
-pkgs='git htop alsamixergui build-essential libasound2-dev cmake libglib2.0-dev autoconf automake libtool lv2-dev xorg-dev libsndfile1-dev libjack-jackd2-dev libsmf-dev gettext'
+pkgs='git htop alsamixergui build-essential libasound2-dev jackd2 cmake libglib2.0-dev autoconf automake libtool lv2-dev xorg-dev libsndfile1-dev libjack-jackd2-dev libsmf-dev gettext'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   read -p "Do you want to install missing packages? " -n 1 -r
   echo
