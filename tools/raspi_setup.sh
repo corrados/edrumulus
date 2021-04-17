df76d892d6d93f3ebe64c0d971de931bff437878
Volker Fischer <corrados@users.noreply.github.com>
2021-04-17 14:02:36 +0200

    exclude some tools directorys, added some dependencies

--- tools/raspi_setup.sh
+++ tools/raspi_setup.sh
@@ -4,7 +4,7 @@ echo "This script prepares a Raspberry Pi for Edrumulus usage"
 
 
 # install required packages ----------------------------------------------------
-pkgs='alsamixergui build-essential qt5-default libasound2-dev cmake libglib2.0-dev autoconf automake libtool lv2-dev xorg-dev libsndfile1-dev libjack-dev libsmf-dev libasound2-dev gettext'
+pkgs='git htop alsamixergui build-essential libasound2-dev cmake libglib2.0-dev autoconf automake libtool lv2-dev xorg-dev libsndfile1-dev libjack-dev libsmf-dev libasound2-dev gettext'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   read -p "Do you want to install missing packages? " -n 1 -r
   echo
