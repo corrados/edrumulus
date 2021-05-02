74b8f13dfbfa92f6c756ae9845721bb2d68afc55
Volker Fischer <corrados@users.noreply.github.com>
2021-05-02 20:23:56 +0200

    settings for the Youtube video

--- tools/run_edrumulus.sh
+++ tools/run_edrumulus.sh
@@ -75,9 +75,9 @@ stty 38400 -F /dev/ttyUSB0
 
 # snare
 echo -n -e '\xB9\x6C\x00' > /dev/ttyUSB0 # select pad: 0
-echo -n -e '\xB9\x66\x07' > /dev/ttyUSB0 # pad type: TP80
-echo -n -e '\xB9\x6F\x03' > /dev/ttyUSB0 # rim/pos: both, rim shot and positional sensing
-echo -n -e '\xB9\x67\x09' > /dev/ttyUSB0 # threshold
+#echo -n -e '\xB9\x66\x07' > /dev/ttyUSB0 # pad type: TP80
+#echo -n -e '\xB9\x6F\x03' > /dev/ttyUSB0 # rim/pos: both, rim shot and positional sensing
+echo -n -e '\xB9\x67\x0A' > /dev/ttyUSB0 # threshold
 #echo -n -e '\xB9\x68\x08' > /dev/ttyUSB0 # sensitivity
 #echo -n -e '\xB9\x6B\x10' > /dev/ttyUSB0 # rim shot threshold
 #echo -n -e '\xB9\x69\x1A' > /dev/ttyUSB0 # positional sensing threshold
@@ -86,13 +86,13 @@ echo -n -e '\xB9\x67\x09' > /dev/ttyUSB0 # threshold
 # kick
 echo -n -e '\xB9\x6C\x01' > /dev/ttyUSB0 # select pad: 1
 echo -n -e '\xB9\x66\x06' > /dev/ttyUSB0 # pad type: KD7
-echo -n -e '\xB9\x67\x09' > /dev/ttyUSB0 # threshold
+echo -n -e '\xB9\x67\x0D' > /dev/ttyUSB0 # threshold
 echo -n -e '\xB9\x68\x09' > /dev/ttyUSB0 # sensitivity
 
 # Hi-Hat
 echo -n -e '\xB9\x6C\x02' > /dev/ttyUSB0 # select pad: 2
-echo -n -e '\xB9\x66\x02' > /dev/ttyUSB0 # pad type: PD8
-echo -n -e '\xB9\x67\x09' > /dev/ttyUSB0 # threshold
+echo -n -e '\xB9\x66\x07' > /dev/ttyUSB0 # pad type: TP80
+echo -n -e '\xB9\x67\x11' > /dev/ttyUSB0 # threshold
 echo -n -e '\xB9\x68\x08' > /dev/ttyUSB0 # sensitivity
 echo -n -e '\xB9\x6F\x01' > /dev/ttyUSB0 # rim/pos: enable rim shot
 
