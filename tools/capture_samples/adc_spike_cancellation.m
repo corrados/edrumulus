136fa2df4a85fb183cc2e9dcf2ae6f407fab4234
Volker Fischer <corrados@users.noreply.github.com>
2021-07-03 19:48:05 +0200

    more testing

--- tools/capture_samples/adc_spike_cancellation.m
+++ tools/capture_samples/adc_spike_cancellation.m
@@ -20,9 +20,15 @@ close all;
 load win_recording1.txt
 x = win_recording1;
 
+%load win_recording_long1.txt
+%x = win_recording_long1;
+
+
 % extract individual processing blocks marked with all zeros
 y          = find(all(x == 0, 2));
 num_blocks = length(y) - 1;
+
+%figure; plot(diff(y));
 
 for i = 1:num_blocks
   z{i} = x(y(i) + 1:y(i + 1) - 1, :);
@@ -30,95 +36,105 @@ end
 
 % select block to analyze
 % interesting blocks: 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24
-block_index = 13;
-x           = z{block_index};
+% interesting blocks long: 20, 22, 106, 170, 194, 201, 202, 239
+block_index_range = 13:15;%6:200;%
 
-% DC offset estimation and compensation
-% we assume here that the DC offset is constand over the entire block
-dc_offset = mean(x);
-x         = x - dc_offset;
+for block_index = block_index_range
+
+  x = z{block_index};
+
+  % DC offset estimation and compensation
+  % we assume here that the DC offset is constand over the entire block
+  dc_offset = mean(x);
+  x         = x - dc_offset;
 
 
 %% TEST for testing we select just one signal
 %x = x(:, 1);
 
 
-for j = 1:size(x, 2)
+  for j = 1:size(x, 2)
 
-  input = x(:, j);
+    input = x(:, j);
 
 
-  % ADC spike cancellation algorithm -------------------------------------------
-  ADC_MAX_RANGE      = 4096; % Teensy 4.0 ADC has 12 bits -> 0..4095
-  ADC_MAX_NOISE_AMPL = 8;    % highest assumed ADC noise amplitude in the ADC input range unit (measured)
+    % ADC spike cancellation algorithm -----------------------------------------
+    ADC_MAX_RANGE      = 4096; % Teensy 4.0 ADC has 12 bits -> 0..4095
+    ADC_MAX_NOISE_AMPL = 8;    % highest assumed ADC noise amplitude in the ADC input range unit (measured)
 
-  ST_OTHER = 0;
-  ST_NOISE = 1;
-  ST_SPIKE = 2;
+    ST_OTHER = 0;
+    ST_NOISE = 1;
+    ST_SPIKE = 2;
 
-  prev_input1       = 0;
-  prev_input2       = 0;
-  prev1_input_state = 0;
-  prev2_input_state = 0;
-  prev3_input_state = 0;
+    prev_input1       = 0;
+    prev_input2       = 0;
+    prev1_input_state = 0;
+    prev2_input_state = 0;
+    prev3_input_state = 0;
 
-  % remove single/dual sample spikes by checking if right before and right after the
-  % detected spike(s) we only have noise and no useful signal (since the ESP32 spikes
-  % mostly are on just one or two sample(s))
-  max_peak_threshold = 150; % maximum assumed ESP32 spike amplitude
+    % remove single/dual sample spikes by checking if right before and right after the
+    % detected spike(s) we only have noise and no useful signal (since the ESP32 spikes
+    % mostly are on just one or two sample(s))
+    max_peak_threshold = 150; % maximum assumed ESP32 spike amplitude
 
-  for i = 1:length(input)
+    for i = 1:length(input)
 
-    return_value = prev_input2; % normal return value in case no spike was detected
-    input_abs    = abs(input(i));
-    input_state  = ST_OTHER; % initialization value, might be overwritten
+      return_value = prev_input2; % normal return value in case no spike was detected
+      input_abs    = abs(input(i));
+      input_state  = ST_OTHER; % initialization value, might be overwritten
 
-    if input_abs < ADC_MAX_NOISE_AMPL
-      input_state = ST_NOISE;
-    elseif input_abs < max_peak_threshold
-      input_state = ST_SPIKE;
+      if input_abs < ADC_MAX_NOISE_AMPL
+        input_state = ST_NOISE;
+      elseif input_abs < max_peak_threshold
+        input_state = ST_SPIKE;
+      end
+
+      % check for single spike sample case
+      if (prev3_input_state == ST_NOISE) && ...
+         (prev2_input_state == ST_SPIKE) && ...
+         (prev1_input_state == ST_NOISE)
+
+        return_value = 0; % remove single spike
+      end
+
+      % check for two sample spike case
+      if (prev3_input_state == ST_NOISE) && ...
+         (prev2_input_state == ST_SPIKE) && ...
+         (prev1_input_state == ST_SPIKE) && ...
+         (input_state       == ST_NOISE)
+
+        prev_input1  = 0; % remove two sample spike
+        return_value = 0; % remove two sample spike
+
+      end
+
+      % update three-step input signal memory where we store the last three states of
+      % the input signal and two previous untouched input samples
+      prev3_input_state = prev2_input_state;
+      prev2_input_state = prev1_input_state;
+      prev1_input_state = input_state;
+      prev_input2       = prev_input1;
+      prev_input1       = x(i);
+
+      x(i, j) = return_value;
+
     end
-
-    % check for single spike sample case
-    if (prev3_input_state == ST_NOISE) && ...
-       (prev2_input_state == ST_SPIKE) && ...
-       (prev1_input_state == ST_NOISE)
-
-      return_value = 0; % remove single spike
-    end
-
-    % check for two sample spike case
-    if (prev3_input_state == ST_NOISE) && ...
-       (prev2_input_state == ST_SPIKE) && ...
-       (prev1_input_state == ST_SPIKE) && ...
-       (input_state       == ST_NOISE)
-
-      prev_input1  = 0; % remove two sample spike
-      return_value = 0; % remove two sample spike
-
-    end
-
-    % update three-step input signal memory where we store the last three states of
-    % the input signal and two previous untouched input samples
-    prev3_input_state = prev2_input_state;
-    prev2_input_state = prev1_input_state;
-    prev1_input_state = input_state;
-    prev_input2       = prev_input1;
-    prev_input1       = x(i);
-
-    x(i, j) = return_value;
 
   end
 
+  % cut out algorithm settling time
+  x = x(3:end, :);
+
+  % plot results
+  plot(x); title(num2str(block_index));
+
+
+% TEST
+if length(block_index_range) > 1
+  pause;
 end
 
-% cut out algorithm settling time
-x = x(3:end, :);
 
-
-
-% plot results
-figure;
-plot(x);
+end
 
 
