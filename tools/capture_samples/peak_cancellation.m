26ecafe819fd7acf90ef10714ba290d42aec5b2b
Volker Fischer <corrados@users.noreply.github.com>
2021-07-03 15:30:47 +0200

    take c++ code as a start

--- tools/capture_samples/peak_cancellation.m
+++ tools/capture_samples/peak_cancellation.m
@@ -34,9 +34,70 @@ block_index = 13;
 x           = z{block_index};
 
 
-% TEST
+% TEST for testing we select just one signal
+x = x(:, 1);
+
+
+% DC offset estimation and compensation
+% we assume here that the DC offset is constand over the entire block
+dc_offset = mean(x);
+x         = x - dc_offset;
+
+
+
+% ADC spike cancellation algorithm ---------------------------------------------
+
+%  // remove single/dual sample spikes by checking if right before and right after the
+%  // detected spike(s) we only have noise and no useful signal (since the ESP32 spikes
+%  // mostly are on just one or two sample(s))
+%  const int max_peak_threshold = 150; // maximum assumed ESP32 spike amplitude
+%
+%  float       return_value = prev_input2[pad_index][input_channel_index]; // normal return value in case no spike was detected
+%  const float input_abs    = abs ( input );
+%  Espikestate input_state  = ST_OTHER; // initialization value, might be overwritten
+%
+%  if ( input_abs < ADC_MAX_NOISE_AMPL )
+%  {
+%    input_state = ST_NOISE;
+%  }
+%  else if ( input_abs < max_peak_threshold )
+%  {
+%    input_state = ST_SPIKE;
+%  }
+%
+%  // check for single spike sample case
+%  if ( ( prev3_input_state[pad_index][input_channel_index] == ST_NOISE ) &&
+%       ( prev2_input_state[pad_index][input_channel_index] == ST_SPIKE ) &&
+%       ( prev1_input_state[pad_index][input_channel_index] == ST_NOISE ) )
+%  {
+%    return_value = 0.0f; // remove single spike
+%  }
+%
+%  // check for two sample spike case
+%  if ( ( prev3_input_state[pad_index][input_channel_index] == ST_NOISE ) &&
+%       ( prev2_input_state[pad_index][input_channel_index] == ST_SPIKE ) &&
+%       ( prev1_input_state[pad_index][input_channel_index] == ST_SPIKE ) &&
+%       ( input_state                                       == ST_NOISE ) )
+%  {
+%    prev_input1[pad_index][input_channel_index] = 0.0f; // remove two sample spike
+%    return_value                                = 0.0f; // remove two sample spike
+%  }
+%
+%  // update three-step input signal memory where we store the last three states of
+%  // the input signal and two previous untouched input samples
+%  prev3_input_state[pad_index][input_channel_index] = prev2_input_state[pad_index][input_channel_index];
+%  prev2_input_state[pad_index][input_channel_index] = prev1_input_state[pad_index][input_channel_index];
+%  prev1_input_state[pad_index][input_channel_index] = input_state;
+%  prev_input2[pad_index][input_channel_index]       = prev_input1[pad_index][input_channel_index];
+%  prev_input1[pad_index][input_channel_index]       = input;
+
+
+
+
+
+
+% plot results
 figure;
 plot(x);
-
 
 
