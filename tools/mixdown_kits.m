8b9cb8eb249c81ee3a6f6ad0687756f0a4bf9a64
Volker Fischer <corrados@users.noreply.github.com>
2021-07-23 19:24:36 +0200

    convert all instruments of a kit

--- tools/mixdown_kits.m
+++ tools/mixdown_kits.m
@@ -45,87 +45,95 @@ mix_matrix = [1, 0; ...
               1, 1; ...
               1, 1];
 
-% TODO loop over all instruments of a kit
-
-% current instrument path
-file_path = 'Tom1_whisker/';
-
-% get instrument XML file name
-instr_root_dir      = dir([kit_path file_path]);
-xml_file_name_index = find(~[instr_root_dir.isdir]);
-[~, xml_file_name]  = fileparts(instr_root_dir(xml_file_name_index).name);
-
-% get instrument samples file names
-instr_samples_dir = dir([kit_path file_path samples_path]);
-
 % initialization
 mkdir(out_kit_path);
 
-% load and modify instrument XML
-file_id           = fopen([kit_path file_path xml_file_name '.xml'], 'r');
-end_of_file_found = false;
-cnt               = 1;
-xml_file          = {};
+% get instrument directory names
+instr_dir = dir(kit_path);
 
-while ~end_of_file_found
-  xml_file{cnt} = fgetl(file_id);
+for instrument_index = 1:length(instr_dir)
 
-  if xml_file{cnt} < 0
-    xml_file          = xml_file(1:end - 1); % cut invalid line
-    end_of_file_found = true;
-  else
+  if instr_dir(instrument_index).isdir && (length(instr_dir(instrument_index).name) > 2)
 
-    % add prefix for mixed signal to the sample name
-    if strfind(xml_file{cnt}, 'sample name="')
-      insert_position = strfind(xml_file{cnt}, 'sample name="') + length('sample name="') - 1;
-      xml_file{cnt}   = [xml_file{cnt}(1:insert_position) mixed_prefix xml_file{cnt}(insert_position + 1:end)];
-    end
+    disp(['Current instrument: ' instr_dir(instrument_index).name]);
 
-    % exchange names of first two channels (left/right channel)
-    if strfind(xml_file{cnt}, ['audiofile channel="' channel_names{1}])
-      xml_file{cnt} = strrep(xml_file{cnt}, ['audiofile channel="' channel_names{1}], ['audiofile channel="left_channel']);
-    end
-    if strfind(xml_file{cnt}, ['audiofile channel="' channel_names{2}])
-      xml_file{cnt} = strrep(xml_file{cnt}, ['audiofile channel="' channel_names{2}], ['audiofile channel="right_channel']);
-    end
+    % get current instrument path
+    file_path = [instr_dir(instrument_index).name '/'];
 
-    % remove all other channels
-    for channel_names_index = 3:length(channel_names)
-      if strfind(xml_file{cnt}, ['audiofile channel="' channel_names{channel_names_index}])
-        cnt = cnt - 1;
+    % get instrument XML file name
+    instr_root_dir      = dir([kit_path file_path]);
+    xml_file_name_index = find(~[instr_root_dir.isdir]);
+    [~, xml_file_name]  = fileparts(instr_root_dir(xml_file_name_index).name);
+
+    % get instrument samples file names
+    instr_samples_dir = dir([kit_path file_path samples_path]);
+
+    % load and modify instrument XML
+    file_id           = fopen([kit_path file_path xml_file_name '.xml'], 'r');
+    end_of_file_found = false;
+    cnt               = 1;
+    xml_file          = {};
+
+    while ~end_of_file_found
+      xml_file{cnt} = fgetl(file_id);
+
+      if xml_file{cnt} < 0
+        xml_file          = xml_file(1:end - 1); % cut invalid line
+        end_of_file_found = true;
+      else
+
+        % add prefix for mixed signal to the sample name
+        if strfind(xml_file{cnt}, 'sample name="')
+          insert_position = strfind(xml_file{cnt}, 'sample name="') + length('sample name="') - 1;
+          xml_file{cnt}   = [xml_file{cnt}(1:insert_position) mixed_prefix xml_file{cnt}(insert_position + 1:end)];
+        end
+
+        % exchange names of first two channels (left/right channel)
+        if strfind(xml_file{cnt}, ['audiofile channel="' channel_names{1}])
+          xml_file{cnt} = strrep(xml_file{cnt}, ['audiofile channel="' channel_names{1}], ['audiofile channel="left_channel']);
+        end
+        if strfind(xml_file{cnt}, ['audiofile channel="' channel_names{2}])
+          xml_file{cnt} = strrep(xml_file{cnt}, ['audiofile channel="' channel_names{2}], ['audiofile channel="right_channel']);
+        end
+
+        % remove all other channels
+        for channel_names_index = 3:length(channel_names)
+          if strfind(xml_file{cnt}, ['audiofile channel="' channel_names{channel_names_index}])
+            cnt = cnt - 1;
+          end
+        end
+
+        cnt = cnt + 1;
+
       end
     end
 
-    cnt = cnt + 1;
+    fclose(file_id);
 
-  end
-end
+    % write modified instrument XML file
+    mkdir([out_kit_path file_path]);
+    file_id = fopen([out_kit_path file_path xml_file_name '.xml'], 'w');
+    for line_index = 1:length(xml_file)
+      fwrite(file_id, xml_file{line_index});
+      fwrite(file_id, 10);
+    end
+    fclose(file_id);
 
-fclose(file_id);
+    % mix and process instrument samples
+    for sample_index = 1:length(instr_samples_dir)
 
-% write modified instrument XML file
-file_id = fopen([out_kit_path file_path xml_file_name '.xml'], 'w');
-for line_index = 1:length(xml_file)
-  fwrite(file_id, xml_file{line_index});
-  fwrite(file_id, 10);
-end
-fclose(file_id);
+      if ~instr_samples_dir(sample_index).isdir
 
-% mix and process instrument samples
-for sample_index = 1:length(instr_samples_dir)
+        % get current sample file name
+        [~, file_name] = fileparts(instr_samples_dir(sample_index).name);
 
-  if ~instr_samples_dir(sample_index).isdir
+        % load wave file
+        x_all = audioread([kit_path file_path samples_path file_name '.wav']);
 
-    % get current sample file name
-    [~, file_name] = fileparts(instr_samples_dir(sample_index).name);
-
-    % load wave file
-    x_all = audioread([kit_path file_path samples_path file_name '.wav']);
-
-    % mix channels
-    x_left  = x_all * mix_matrix(:, 1);
-    x_right = x_all * mix_matrix(:, 2);
-    x       = [x_left, x_right];
+        % mix channels
+        x_left  = x_all * mix_matrix(:, 1);
+        x_right = x_all * mix_matrix(:, 2);
+        x       = [x_left, x_right];
 
 
 % TEST
@@ -135,14 +143,19 @@ for sample_index = 1:length(instr_samples_dir)
 %freqz(b, a);
 %x = filter(b, a, x);
 
+% TEST
+%% play the resulting wave form
+%player = audioplayer(x, sampling_rate, 16);
+%play(player);
 
-    % play the resulting wave form
-    player = audioplayer(x, sampling_rate, 16);
-    play(player);
 
-    % store the resulting wave file
-    mkdir([out_kit_path file_path samples_path]);
-    audiowrite([out_kit_path file_path samples_path mixed_prefix file_name '.wav'], x, sampling_rate);
+        % store the resulting wave file
+        mkdir([out_kit_path file_path samples_path]);
+        audiowrite([out_kit_path file_path samples_path mixed_prefix file_name '.wav'], x, sampling_rate);
+
+      end
+
+    end
 
   end
 
