f8677b57a0fd07f01c4d796244e248601fd7728f
Volker Fischer <corrados@users.noreply.github.com>
2021-12-09 16:42:39 +0100

    use a low pass filter of the rim signal to improve detection

--- algorithm/signalsandsettings.m
+++ algorithm/signalsandsettings.m
@@ -78,6 +78,7 @@ pad.decay_grad_fact3          = 200;
 pad.pos_low_pass_cutoff       = 150; % Hz
 pad.pos_invert                = false;
 pad.rim_shot_window_len_ms    = 3.5;
+pad.rim_low_pass_iir_alpha    = 700;
 
 switch padtype
   case 'pd120'
