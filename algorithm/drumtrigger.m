e49fa5be60e22e6f25dd02a8e23c230b2fa2f074
Volker Fischer <corrados@users.noreply.github.com>
2021-12-22 17:49:16 +0100

    introduce middle metric for hot spot detection

--- algorithm/drumtrigger.m
+++ algorithm/drumtrigger.m
@@ -55,9 +55,9 @@ plot(all_first_peaks, 10 * log10(x(all_first_peaks, 1) .^ 2), 'b*');
 
 
 % TEST
-10 * log10(hot_spot_metric)
+%10 * log10(hot_spot_metric)
 %hot_spot_metric((10 * log10(hot_spot_metric) < 0.4) | (10 * log10(hot_spot_metric) > 2.5)) = nan;
-hot_spot_metric((10 * log10(hot_spot_metric) < 4) | (10 * log10(hot_spot_metric) > 5)) = nan;
+%hot_spot_metric((10 * log10(hot_spot_metric) < 4) | (10 * log10(hot_spot_metric) > 5)) = nan;
 
 plot(all_second_peaks, 10 * log10(x(all_second_peaks, 1) .^ 2), 'm*');
 plot(all_second_peaks, hot_spot_metric + 70, 'c*', "markersize", 15);
@@ -209,17 +209,42 @@ while ~no_more_peak
 
 % TEST hot spot detection testing
 second_peak_diff = round(2.55 * 1e-3 * Fs);
-hot_spot_win_len = 10;
+hot_spot_win_len = 6;
 
 second_peak_range = above_thresh_start + max_idx + second_peak_diff - 1 + (-hot_spot_win_len / 2:hot_spot_win_len / 2);
 [second_peak_value, second_peak_idx] = max(x_sq(second_peak_range));
 second_peak_idx = second_peak_idx + second_peak_range(1) - 1;
 
+middle_range_len   = 6;
+middle_range       = max_idx + ( second_peak_idx - max_idx ) / 2 + (-middle_range_len / 2:middle_range_len / 2);
+middle_range_power = mean(x_sq(round(middle_range)));
+middle_range_metric = x_sq(peak_idx) / middle_range_power;
+
 %second_peak_idx  = above_thresh_start + max_idx + second_peak_diff - 1;
 %second_peak_value = x_sq(second_peak_idx);
 
-all_second_peaks = [all_second_peaks; second_peak_idx];
-hot_spot_metric  = [hot_spot_metric; x_sq(peak_idx) / x_sq(second_peak_idx)];
+
+limit_min = 4; limit_max = 5; % PD80R
+%limit_min = 0.4; limit_max = 2.5; % PD120
+
+%hot_spot_metric((10 * log10(hot_spot_metric) < 0.4) | (10 * log10(hot_spot_metric) > 2.5)) = nan;
+%hot_spot_metric((10 * log10(hot_spot_metric) < 4) | (10 * log10(hot_spot_metric) > 5)) = nan;
+
+x11=x_sq(peak_idx) / x_sq(second_peak_idx);
+if ~((10 * log10(x11) < limit_min) | (10 * log10(x11) > limit_max))
+  10 * log10(middle_range_metric)
+
+  if 10 * log10(middle_range_metric) < 50
+
+    all_second_peaks = [all_second_peaks; second_peak_idx];
+    hot_spot_metric  = [hot_spot_metric; x_sq(peak_idx) / x_sq(second_peak_idx)];
+
+  end
+
+end
+
+
+
 
 
   % search from above threshold to corrected scan+mask time for highest peak in
