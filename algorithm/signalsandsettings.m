8d4219124e8637d79a88490e9cfd6e0e85a46c58
Volker Fischer <corrados@users.noreply.github.com>
2022-01-07 21:03:53 +0100

    small changes

--- algorithm/signalsandsettings.m
+++ algorithm/signalsandsettings.m
@@ -42,7 +42,7 @@ padtype = 'pd120'; % default
 %x = audioread("signals/pd120_rimshot.wav");%x=x(7000:15000,:);%x = x(1:100000, :);%x = x(168000:171000, :);%x = x(1:34000, :);%
 %x = audioread("signals/pd120_rimshot_hardsoft.wav");
 %x=audioread("signals/pd120_middle_velocity.wav");x=[x;audioread("signals/pd120_pos_sense2.wav")];x=[x;audioread("signals/pd120_hot_spot.wav")];
-%x = audioread("signals/pd80r.wav");x=x(:,1);padtype='pd80r';x = x(1:265000, :);%x = x(264000:320000, :);%
+x = audioread("signals/pd80r.wav");x=x(:,1);padtype='pd80r';x = x(1:265000, :);%x = x(264000:320000, :);%
 %x = audioread("signals/pd80r_hot_spot.wav");padtype='pd80r';%x = x(191700:192400, :);%
 %x = audioread("signals/pd80r_no_hot_spot.wav");padtype='pd80r';
 %x = audioread("signals/pd80r_rimshot_issue.wav");padtype='pd80r';
@@ -59,7 +59,7 @@ padtype = 'pd120'; % default
 %x = audioread("signals/kd7_hard_hits.wav");padtype = 'kd7';x = x(1:3000, :);
 %x = audioread("signals/tp80.wav");padtype = 'tp80';
 %x = audioread("signals/vh12.wav");padtype = 'vh12';%x = x(900000:end, :);%x = x(376000:420000, :);%x = x(1:140000, :);
-x = audioread("signals/drumtec_diabolo12.wav");x=x(:,1);padtype='diabolo12';
+%x = audioread("signals/drumtec_diabolo12.wav");x=x(:,1);padtype='diabolo12';
 
 
 % scale to the ESP32 input range to match the signal level of the ESP32
