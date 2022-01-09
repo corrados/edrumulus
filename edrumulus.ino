1a64b6cb530046c055389a30a954d27a9b30ee77
Volker Fischer <corrados@users.noreply.github.com>
2022-01-09 11:16:27 +0100

    prototype 3 (HD1) configuration

--- edrumulus.ino
+++ edrumulus.ino
@@ -82,21 +82,21 @@ void setup()
   edrumulus.set_midi_notes      ( 8, 43, 58 ); // tom 3
 
 // my prototype setup configuration...
-  edrumulus.set_pad_type          ( 0, Edrumulus::PD80R ); // snare
-  edrumulus.set_rim_shot_is_used  ( 0, true );
+  edrumulus.set_pad_type          ( 0, Edrumulus::PDX8 ); // snare
+  //edrumulus.set_rim_shot_is_used  ( 0, true ); // no rim shot piezo on HD1-PDX8
   edrumulus.set_pos_sense_is_used ( 0, true );
   edrumulus.set_pad_type          ( 1, Edrumulus::KD7 ); // kick
   edrumulus.set_curve             ( 1, Edrumulus::LOG2 ); // less dynamic on kick (similar to other drum modules)
-  edrumulus.set_pad_type          ( 2, Edrumulus::PD8 ); // Hi-Hat, using rim switch
-  edrumulus.set_rim_shot_is_used  ( 2, true );
+  edrumulus.set_pad_type          ( 2, Edrumulus::CY5 ); // Hi-Hat, using rim switch
+  //edrumulus.set_rim_shot_is_used  ( 2, true ); // no rim shot piezo on HD1-CY5
   edrumulus.set_pad_type          ( 3, Edrumulus::FD8 ); // Hi-Hat-ctrl
-  edrumulus.set_pad_type          ( 4, Edrumulus::CY8 ); // crash, using rim switch
-  edrumulus.set_rim_shot_is_used  ( 4, true );
+  edrumulus.set_pad_type          ( 4, Edrumulus::CY5 ); // crash, using rim switch
+  //edrumulus.set_rim_shot_is_used  ( 4, true ); // no rim shot piezo on HD1-CY5
   edrumulus.set_cancellation      ( 4, 4 ); // avoid that kick triggers crash
-  edrumulus.set_pad_type          ( 5, Edrumulus::TP80 ); // tom 1
-  edrumulus.set_pad_type          ( 6, Edrumulus::PD8 ); // ride, using rim switch
-  edrumulus.set_rim_shot_is_used  ( 6, true );
-  edrumulus.set_pad_type          ( 7, Edrumulus::PD8 ); // tom 2
+  edrumulus.set_pad_type          ( 5, Edrumulus::HD1TOM ); // tom 1
+  edrumulus.set_pad_type          ( 6, Edrumulus::CY5 ); // ride, using rim switch
+  //edrumulus.set_rim_shot_is_used  ( 6, true ); // no rim shot piezo on HD1-CY5
+  edrumulus.set_pad_type          ( 7, Edrumulus::HD1TOM ); // tom 2
   //edrumulus.set_pad_type          ( 8, Edrumulus::PD8 ); // tom 3
 
   // initialize GPIO port for status LED
