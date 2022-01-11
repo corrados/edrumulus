3f951e0ce93172518b709378f641936c9bbd7443
Volker Fischer <corrados@users.noreply.github.com>
2022-01-11 18:41:02 +0100

    added initial version of simple terminal GUI

---
+++ tools/edrumulus_gui.c
@@ -0,0 +1,87 @@
+
+// Edrumulus simple terminal GUI
+// compile with: gcc edrumulus_gui.c -o gui -lncurses
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <curses.h>
+
+unsigned char* get_midi_cmd();
+
+int main()
+{
+  WINDOW* mainwin;
+
+  int ch;
+  int serial_port;
+  struct termios tty;
+
+  // open serial USB port and set correct baud rate
+  serial_port = open("/dev/ttyUSB0", O_RDWR);
+
+  if ( tcgetattr ( serial_port, &tty ) != 0 )
+  {
+    fprintf ( stderr, "Error from tcgetattr.\n" );
+    exit ( EXIT_FAILURE );
+  }
+
+  cfsetispeed ( &tty, B38400 );
+  cfsetospeed ( &tty, B38400 );
+
+  if ( tcsetattr ( serial_port, TCSANOW, &tty ) != 0 )
+  {
+    fprintf ( stderr, "Error from tcsetattr.\n" );
+    exit ( EXIT_FAILURE );
+  }
+
+
+  // initialize GUI
+  mainwin = initscr();
+  noecho();                 // turn off key echoing
+  keypad ( mainwin, true ); // enable the keypad for non-char keys
+
+  // show usage
+  mvaddstr ( 5, 10, "Press a key, q:quit, p:sel pad, a:MIDI curve" );
+  refresh();
+  move(7, 10);
+
+  // loop until user presses q
+  while ( ( ch = getch() ) != 'q' )
+  {
+    // delete the old response line, and print a new one
+    deleteln();
+
+/* TEST */
+if ( ch == 'p' )
+{
+  mvprintw ( 7, 10, "p:sel pad" );
+  write(serial_port, get_midi_cmd(), 3);
+}
+if ( ch == 'a' )
+{
+  mvprintw ( 7, 10, "a:MIDI curve" );
+  unsigned char midi_cmd[] = "\xB9\x6D\x03";
+  write(serial_port, midi_cmd, 3);
+}
+
+    refresh();
+  }
+
+  // clean up and exit
+  delwin ( mainwin );
+  endwin();
+  refresh();
+  close ( serial_port );
+  return EXIT_SUCCESS;
+}
+
+// utility function to get current MIDI command
+unsigned char* get_midi_cmd()
+{
+  static unsigned char midi_cmd[3] = "\xB9\x6C\x00";
+  return midi_cmd;
+}
+
