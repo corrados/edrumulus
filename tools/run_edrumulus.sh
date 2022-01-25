d17218311ba47b5eb7baa570475ba9c71f8a2006
Volker Fischer <corrados@users.noreply.github.com>
2022-01-25 19:03:28 +0100

    add jack2 library for Raspberry Pi

--- tools/run_edrumulus.sh
+++ tools/run_edrumulus.sh
@@ -8,7 +8,7 @@
 #           sudo sh -c 'echo "@audio   -  rtprio   95" >> /etc/security/limits.conf'
 #           sudo sh -c 'echo "@audio   -  memlock  unlimited" >> /etc/security/limits.conf'
 
-echo "This script prepares a Raspberry Pi for Edrumulus usage"
+echo "This script prepares a Linux/Raspberry Pi system for Edrumulus usage"
 
 
 # get environment --------------------------------------------------------------
@@ -65,6 +65,34 @@ else
   cd Builds/LinuxMakefile
   make
   cd ../../..
+fi
+
+
+# download and compile jack2 (optional) ----------------------------------------
+if [[ -v is_raspi ]]; then
+  # Jack audio without DBUS support
+  if [ -d "jack2" ]; then
+    echo "The Jack2 directory is present, we assume it is compiled and ready to use. If not, delete the jack2 directory and call this script again."
+  else
+    git clone https://github.com/jackaudio/jack2.git
+    cd jack2
+    git checkout v1.9.20
+    ./waf configure --alsa --prefix=/usr/local --libdir=$(pwd)/build
+    ./waf -j${NCORES}
+    mkdir build/jack
+    cp build/*.so build/jack
+    cp build/common/*.so build/jack
+    cp build/example-clients/*.so build/jack
+    cd ..
+
+    # give audio group rights to do realtime
+    if grep -Fq "@audio" /etc/security/limits.conf; then
+      echo "audio group already has realtime rights"
+    else
+      sudo sh -c 'echo "@audio   -  rtprio   95" >> /etc/security/limits.conf'
+      sudo sh -c 'echo "@audio   -  memlock  unlimited" >> /etc/security/limits.conf'
+    fi
+  fi
 fi
 
 
