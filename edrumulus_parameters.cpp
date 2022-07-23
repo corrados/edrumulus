00c7db271827643d6a1efc0f0ab67c39f741fa5b
Volker Fischer <corrados@users.noreply.github.com>
2022-07-23 21:02:51 +0200

    update the PDA120LS parameters

--- edrumulus_parameters.cpp
+++ edrumulus_parameters.cpp
@@ -61,10 +61,15 @@ void Edrumulus::Pad::apply_preset_pad_settings()
       break;
 
     case PDA120LS:
+      pad_settings.velocity_threshold        = 11;
+      pad_settings.velocity_sensitivity      = 7;
+      pad_settings.pos_threshold             = 31;
+      pad_settings.pos_sensitivity           = 16;
+      pad_settings.rim_shot_treshold         = 30;
       pad_settings.decay_grad_fact2          = 250.0f;
       pad_settings.pre_scan_time_ms          = 3.5f;
       pad_settings.first_peak_diff_thresh_db = 7.0f;
-      pad_settings.pos_low_pass_cutoff       = 370.0f;
+      pad_settings.pos_invert                = true;
       break;
 
     case PD80R:
