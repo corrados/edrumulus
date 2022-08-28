053b74187e1b9caaa7cdc6cbd24f1f7ece595dd2
Volker Fischer <corrados@users.noreply.github.com>
2022-08-28 14:51:44 +0200

    some fixes for front panel script

--- tools/front_panel/gui.py
+++ tools/front_panel/gui.py
@@ -7,6 +7,7 @@ client       = jack.Client('edrumulus_front_panel')
 port_in      = client.midi_inports.register('input')
 port_out     = client.midi_outports.register('output')
 port_through = client.midi_outports.register('through')
+database     = [0] * 128;
 
 @client.set_process_callback
 def process(frames):
@@ -14,10 +15,17 @@ def process(frames):
   port_through.clear_buffer()
   for offset, data in port_in.incoming_midi_events():
     port_through.write_midi_event(offset, data) # pass through
+    if len(data) == 3:
+      if int.from_bytes(data[0], "big") == 0x80:
+        key   = int.from_bytes(data[1], "big")
+        value = int.from_bytes(data[2], "big")
+        database[key] = value
 
-    # for debugging
-    print('{}: 0x{}'.format(client.last_frame_time + offset,
-                                binascii.hexlify(data).decode()))
+      # for debugging
+      #print('{}, {}'.format(key, value))
+      print('{}: 0x{}'.format(client.last_frame_time + offset,
+                              binascii.hexlify(data).decode()))
+      print(database)
 
 with client:
   print('press Return to quit')
