f29c9f82ab891044d3721ca7b946d3af252f6418
Volker Fischer <corrados@users.noreply.github.com>
2022-09-10 12:37:14 +0200

    added some untested code in gui.py for LCD and button operation

--- tools/front_panel/gui.py
+++ tools/front_panel/gui.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 
+# required libraries: sudo pip install RPLCD
+
 import jack
 import binascii
+import RPi.GPIO as GPIO
+from RPLCD.gpio import CharLCD
 
+# general initializations
+database = [0] * 128;
+
+# init jack
 client       = jack.Client('edrumulus_front_panel')
 port_in      = client.midi_inports.register('input')
 port_out     = client.midi_outports.register('output')
 port_through = client.midi_outports.register('through')
-database     = [0] * 128;
+
+# init 16x2 LCD
+lcd = CharLCD(pin_rs=27, pin_rw=None, pin_e=17, pins_data=[22, 23, 24, 10],
+              numbering_mode=GPIO.BOARD, cols=16, rows=2)
+
+# init buttons
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(25, GPIO.RISING, callback=button_handler, bouncetime=10)
+GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(11, GPIO.RISING, callback=button_handler, bouncetime=10)
+GPIO.setup(8, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(8, GPIO.RISING, callback=button_handler, bouncetime=10)
+GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(7, GPIO.RISING, callback=button_handler, bouncetime=10)
+GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(12, GPIO.RISING, callback=button_handler, bouncetime=10)
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(13, GPIO.RISING, callback=button_handler, bouncetime=10)
+
+def button_handler(pin):
+  print("pin %s's value is %s" % (pin, GPIO.input(pin)))
 
 @client.set_process_callback
 def process(frames):
@@ -29,6 +58,14 @@ def process(frames):
 
 with client:
   print('press Return to quit')
+
+  # testing LCD
+  lcd.clear()
+  lcd.cursor_pos = (1, 4)
+  lcd.write_string('Edrumulus')
+  lcd.cursor_pos = (2, 0)
+  lcd.write_string('press Return to quit')
+
   port_in.connect('ttymidi:MIDI_in')
   port_out.connect('ttymidi:MIDI_out')
   port_out.write_midi_event(0, (185, 108, 0))
