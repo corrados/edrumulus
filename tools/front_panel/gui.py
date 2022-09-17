b534a8d38a28152d8aac9d4d5ea20d1ff197268e
Volker Fischer <corrados@users.noreply.github.com>
2022-09-17 21:43:39 +0200

    added tables in front panel Python script

--- tools/front_panel/gui.py
+++ tools/front_panel/gui.py
@@ -7,6 +7,26 @@ import binascii
 import RPi.GPIO as GPIO
 from RPLCD.gpio import CharLCD
 
+# tables
+enum_pad_types = ["PD120", "PD80R", "PD8", "FD8", "VH12", "VH12CTRL", "KD7", "TP80", "CY6", "CY8",
+  "DIABOLO12", "CY5", "HD1TOM", "PD6", "KD8", "PDX8", "KD120", "PD5", "PDA120LS", "PDX100", "KT10"]
+enum_curve_types = ["LINEAR", "EXP1", "EXP2", "LOG1", "LOG2"]
+enum_pad_names = ["snare", "kick", "hi-hat", "ctrl", "crash", "tom1", "ride", "tom2", "tom3"]
+settings_tab = [ # [settings name], [MIDI note], [settings range]
+  ["type",      102,  20],
+  ["thresh",    103,  31],
+  ["sens",      104,  31],
+  ["pos thres", 105,  31],
+  ["pos sens",  106,  31],
+  ["rim thres", 107,  31],
+  ["curve",     109,   4],
+  ["spike",     110,   4],
+  ["rim/pos",   111,   3],
+  ["note",      112, 127],
+  ["note rim",  113, 127],
+  ["cross",     114,  31] ]
+button_pin = {25: 0, 11: 1, 8: 2, 7: 3, 12: 4, 13: 5}
+
 # general initializations
 database = [0] * 128;
 
@@ -17,25 +37,20 @@ port_out = client.midi_outports.register('output')
 
 # init 16x2 LCD
 lcd = CharLCD(pin_rs = 27, pin_rw = None, pin_e = 17, pins_data = [22, 23, 24, 10],
-              numbering_mode = GPIO.BCM, cols = 16, rows = 2)
+              numbering_mode = GPIO.BCM, cols = 16, rows = 2, auto_linebreaks = False)
 
 def button_handler(pin):
   if GPIO.input(pin) == 1:
     lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    if pin == 25:
-      lcd.write_string("Button 1")
-    if pin == 11:
-      lcd.write_string("Button 2")
-    if pin == 8:
-      lcd.write_string("Button 3")
-    if pin == 7:
-      lcd.write_string("Button 4")
-    if pin == 12:
-      lcd.write_string("Button 5")
-    if pin == 13:
-      lcd.write_string("Button 6")
+
+    # TEST show pressed button value on console
     print("pin %s's value is %s" % (pin, GPIO.input(pin)))
+
+    # TEST show dummy outputs on a button press
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string("%d: setting" % button_pin[pin])
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("%s" % settings_tab[button_pin[pin]])
 
 @client.set_process_callback
 def process(frames):
@@ -75,7 +90,7 @@ with client:
   lcd.clear()
   lcd.cursor_pos = (0, 3)
   lcd.write_string('Edrumulus')
-  lcd.cursor_pos = (1, 0)
+  lcd.cursor_pos = (1, 2)
   lcd.write_string('Prototype 5')
 
   port_in.connect('ttymidi:MIDI_in')
