3b70ac799103d6cab7ee800315f26b97d0c120fe
Volker Fischer <corrados@users.noreply.github.com>
2022-09-18 09:29:24 +0200

    add first test with flash_id of flash_esp32 script

--- tools/flash_esp32.sh
+++ tools/flash_esp32.sh
@@ -30,16 +30,30 @@ fi
 
 
 # prepare the ESP32 for flashing -----------------------------------------------
+# We assume that the following hardware wiring is used:
+# PI-GPIO9 -> ESP32 EN
+# PI-GPIO4 -> ESP32 IO0
 sudo systemctl start pigpiod
+pigs modes 9 w
+pigs modes 4 w
 
-pigs r 18
+# to flash, set IO0 to low and reset ESP32
+pigs w 4 0
+pigs w 9 0
+sleep 1
+pigs w 9 1
 
-
-#esptool.py -p /dev/serial0 flash_id
+esptool.py -p /dev/serial0 flash_id
 
 #esptool.py -p /dev/serial0 erase_flash
 
 #esptool.py --chip esp32 --port /dev/serial0 write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_dio_80m.bin 0x10000 edrumulus.ino.bin 0x8000 edrumulus.ino.partitions.bin 
+
+# set IO0 to non-flash mode and reboot ESP32
+pigs w 4 1
+pigs w 9 0
+sleep 1
+pigs w 9 1
 
 
 
