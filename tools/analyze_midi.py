76ab6d03d9376296c138cc657dd73672d1b9a3f9
Volker Fischer <corrados@users.noreply.github.com>
2022-10-08 20:57:50 +0200

    analyze_midi.py is now ready and replaces ananlyze_midi.m

--- tools/analyze_midi.py
+++ tools/analyze_midi.py
@@ -19,86 +19,53 @@
 
 import time
 import jack
-import matplotlib
-# ['GTK3Agg', 'GTK3Cairo', 'GTK4Agg', 'GTK4Cairo', 'MacOSX', 'nbAgg', 'QtAgg', 'QtCairo', 'Qt5Agg', 'Qt5Cairo', 'TkAgg', 
-# 'TkCairo', 'WebAgg', 'WX', 'WXAgg', 'WXCairo', 'agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template']
-
-#matplotlib.use('QtAgg')
-#from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
-import matplotlib.markers as mkr
-import numpy as np
-#%matplotlib notebook
 
 
 # initializations
-client                 = jack.Client('edrumulus_analyze_midi')
-port_in                = client.midi_inports.register('input')
-port_out               = client.midi_outports.register('output')
-midi_send_cmd          = -1 # invalidate per default
-midi_previous_send_cmd = -1
-midi_send_val          = 0
-new_data               = False
-key                    = 0
-value                  = 0
-midi_values            = [0] * 100
+client      = jack.Client('edrumulus_analyze_midi')
+port_in     = client.midi_inports.register('input')
+midi_values = [0] * 100
+midi_pos    = [0] * 100
 
 
 @client.set_process_callback
 def process(frames):
-  global key, value, new_data, midi_values
   for offset, data in port_in.incoming_midi_events():
     if len(data) == 3:
-      if int.from_bytes(data[0], "big") & 0xF0 == 0x90:
-        key   = int.from_bytes(data[1], "big")
+      if int.from_bytes(data[0], "big") & 0xF0 == 0x90: # note on
         value = int.from_bytes(data[2], "big")
-        #midi_values.put(value)
-
         midi_values.pop(0)
         midi_values.append(value)
 
-        #new_data = True
-        #while new_data:
-        #  time.sleep(0.001)
+      if int.from_bytes(data[0], "big") & 0xF0 == 0xB0: # controller
+        key   = int.from_bytes(data[1], "big")
+        value = int.from_bytes(data[2], "big")
+        if key == 16: # positional sensing
+          midi_pos.pop(0)
+          midi_pos.append(value)
 
 
 with client:
-  print('press Return to quit')
+  print('close plot window to quit')
   port_in.connect('ttymidi:MIDI_in')
-  port_out.connect('ttymidi:MIDI_out')
+  fig, (ax0, ax1) = plt.subplots(2, 1)
+  fignum          = fig.number
+  plt.ion()
 
-  #an = np.linspace(0, 2 * np.pi, 100)
-  #plt.plot(3 * np.cos(an), 3 * np.sin(an))
-  #plt.plot((1, 2), marker="o")
-  #plt.scatter([1, 1], [1, -2])
-  #plt.show(block=False)
-
-  test = [1, 2, 3, 4]
-  print(test)
-  print(test.pop(0))
-  print(test)
-  test.append(22)
-  print(test)
-
-  for i in range(0, 50):
-    print(midi_values)
-    plt.ion()
-    print(plt.isinteractive())
-    print(plt.get_backend())
-    plt.clf()
-    plt.plot(midi_values)
-    #plt.draw()
+  while True:
+    ax0.cla()
+    ax0.set_title('MIDI Velocity')
+    ax0.plot(midi_values)
+    ax0.set_ylim(0, 127)
+    ax0.grid(True)
+    ax1.cla()
+    ax1.set_title('MIDI Position')
+    ax1.plot(midi_pos)
+    ax1.set_ylim(0, 127)
+    ax1.grid(True)
     plt.show()
-    plt.pause(0.5)
-#    time.sleep(1)
-
-  #while True:
-  #  if new_data:
-  #    # TODO
-  #    new_data = False
-  #    #print(midi_values)
-  #  time.sleep(0.01)
-
-  # TEST
-  #input()
+    plt.pause(0.1)
+    if not plt.fignum_exists(fignum): # if plot window is closed then quit
+      break
 
