0151e286e70776fcd56b521969479846ccd895c5
Volker Fischer <corrados@users.noreply.github.com>
2022-10-31 19:39:58 +0100

    added new file pos_det.py

---
+++ algorithm/pos_det.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+#*******************************************************************************
+# Copyright (c) 2022-2022
+# Author(s): Volker Fischer
+#*******************************************************************************
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+#*******************************************************************************
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+
+# initializations
+print('close plot window to quit')
+click_point  = (0.5, 0.5)
+sensor1      = np.array(0.5, dtype=complex)
+sensor1.imag = 1;
+sensor2      = np.array(0.066987, dtype=complex)
+sensor2.imag = 0.25;
+sensor3      = np.array(0.933, dtype=complex)
+sensor3.imag = 0.25;
+fig          = plt.figure(tight_layout=True)
+gs           = gridspec.GridSpec(1, 1)
+ax0          = fig.add_subplot(gs[0])
+fignum       = fig.number
+plt.ion()
+
+def onclick(event):
+  global click_point
+  click_point = (event.xdata, event.ydata)
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+while True:
+  ax0.cla()
+
+  # pad edge boundary circle
+  ax0.add_patch(mpatches.Circle((0.5, 0.5), 0.5, fill=False, linewidth=4))
+
+  # sensors
+  plt.scatter(sensor1.real, sensor1.imag, marker="x", c="r", s=100)
+  plt.scatter(sensor2.real, sensor2.imag, marker="x", c="r", s=100)
+  plt.scatter(sensor3.real, sensor3.imag, marker="x", c="r", s=100)
+
+  # create current mouse click vector where the center of the circle is the origin
+  click_vector      = np.array(click_point[0], dtype=complex)
+  click_vector.imag = click_point[1];
+
+  l1 = np.abs(click_vector - sensor1)
+  l2 = np.abs(click_vector - sensor2)
+  l3 = np.abs(click_vector - sensor3)
+
+  l21 = l2 - l1
+  l31 = l3 - l1
+  l32 = l3 - l2
+
+  # approximation of distance: r = max(|L21|, |L31|, |L32|) (using some magic factor of 0.7)
+  r_est = np.max(np.array([np.abs(l21), np.abs(l31), np.abs(l32)])) * 0.7
+
+  ax0.add_patch(mpatches.Circle((0.5, 0.5), np.abs(click_vector - (0.5 + 0.5j)), fill=False, color="b", linestyle='--'))
+  ax0.add_patch(mpatches.Circle((0.5, 0.5), r_est, fill=False, color="b"))
+
+  plt.show()
+  plt.pause(0.3)
+  if not plt.fignum_exists(fignum): # if plot window is closed then quit
+    break
+
