fb2ea334d1635cf061fdca8dd6d1ad6136a0be20
Volker Fischer <corrados@users.noreply.github.com>
2022-11-30 17:04:06 +0100

    code cleanup: remove all unused algorithms

--- tools/create_drumgizmo_kit.py
+++ tools/create_drumgizmo_kit.py
@@ -23,8 +23,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 from scipy.io import wavfile
-from scipy.signal import butter, sosfilt, lfilter, firwin
-from scipy.ndimage import uniform_filter1d
 
 
 ################################################################################
@@ -43,27 +41,16 @@ instruments = [["kick",          "KDrum",   36, 46], \
                ["ride",          "OHRight", 51, 68], \
                ["ride_bell",     "OHRight", 53, 60]]
 
-# TEST for optimizing the analization algorithms, only use the snare drum
-#instruments = [["kick",          "KDrum",   36, 46]]
-#instruments = [["snare",         "Snare",   38, 70]]
-#instruments = [["snare_rimshot", "Snare",   40, 60]]
-#instruments = [["hihat_closed",  "Hihat",   22, 68]] # some strikes not detected
-#instruments = [["hihat_open",    "Hihat",   26, 53]]
-#instruments = [["tom1",          "Tom1",    48, 60]]
-#instruments = [["tom2",          "Tom2",    45, 60]]
-#instruments = [["tom3",          "Tom3",    43, 57]]
-#instruments = [["crash",         "OHLeft",  55, 60]]
-#instruments = [["ride",          "OHRight", 51, 68]]
-#instruments = [["ride_bell",     "OHRight", 53, 60]]
+# TEST for optimizing the analization algorithms, only use one instrument
+instruments = [instruments[0]]
 
-kit_name                  = "PearlMMX" # avoid spaces
-samples_dir_name          = "samples" # compatible to other Drumgizmo kits
-source_samples_dir_name   = "source_samples" # root directory of recorded source samples
-kit_description           = "Pearl MMX drum set with positional sensing support"
-channel_names             = ["KDrum", "Snare", "Hihat", "Tom1", "Tom2", "Tom3", "OHLeft", "OHRight"]
-min_pause_between_strikes = 1.5 # seconds
-min_on_detection_gap      = 0.25 # seconds
-num_channels              = len(channel_names)
+kit_name                = "PearlMMX" # avoid spaces
+samples_dir_name        = "samples" # compatible to other Drumgizmo kits
+source_samples_dir_name = "source_samples" # root directory of recorded source samples
+kit_description         = "Pearl MMX drum set with positional sensing support"
+channel_names           = ["KDrum", "Snare", "Hihat", "Tom1", "Tom2", "Tom3", "OHLeft", "OHRight"]
+min_strike_len          = 0.25 # seconds
+num_channels            = len(channel_names)
 
 
 for instrument in instruments:
@@ -119,108 +106,34 @@ for instrument in instruments:
   ##############################################################################
   # WAVE FORM ANALYSIS #########################################################
   ##############################################################################
-  # analyze master channel and find strikes
   thresh_from_max = instrument[3] # dB from maximum peak
   master_channel  = channel_names.index(instrument[1])
 
-  x = np.square(sample_float[master_channel])
-
-  ## TEST linear regression -> instruments = [["hihat_open", "Hihat", 26]]
-  #s = 2200000
-  #e = 2800000
-  #y = x[range(s, e)]
-  #y = sosfilt(butter(2, 0.001, btype="low", output="sos"), y)
-  #lr_x_values = range(s, e)
-  #A = np.vstack([lr_x_values, np.ones(len(lr_x_values))]).T
-  #m, c = np.linalg.lstsq(A, 10 * np.log10(np.abs(y)), rcond=None)[0]
-  #plt.plot(10 * np.log10(np.abs(x)))
-  #plt.plot(lr_x_values, m * lr_x_values + c, 'r')
-  #plt.show()
-
-  #x = sosfilt(butter(2, 0.001, btype="low", output="sos"), x)
-
-  ## TEST find peaks
-  #max_index = np.argmax(x)
-  #peak = x[max_index - 10000:max_index + 10000]
-  #x = np.correlate(x, peak)
-  #plt.plot(10 * np.log10(np.abs(x)))
-  #plt.show()
-
-
-  #x = sosfilt(butter(2, 0.0001, btype="low", output="sos"), x)
-
-  #alpha = 0.999 # 0.99985
-  #x = lfilter((1 - alpha, 0), (1, -alpha), x)
-  #x = np.roll(x, -100)
-
-  #fir = firwin(3000, 0.00000001)
-  #x   = lfilter(fir, 1.0, x)
-
-  threshold = np.power(10, (10 * np.log10(np.max(x)) - thresh_from_max) / 10)
-
-  ## detect strikes using a mask time (minimum pause between strikes in recording)
-  #above_thresh   = np.full(len(x), False)
-  #last_above_idx = -10 * sample_rate # initialize with 10 seconds in the past
-  #for i in range(0, len(x)):
-  #  if x[i] > threshold and i - last_above_idx > min_pause_between_strikes * sample_rate:
-  #    above_thresh[i] = True
-  #    last_above_idx  = i
-
+  # find samples which are above the threshold
+  x            = np.square(sample_float[master_channel])
+  threshold    = np.power(10, (10 * np.log10(np.max(x)) - thresh_from_max) / 10)
   above_thresh = x > threshold
 
-  #N = 2000
-  ##above_thresh = np.convolve(above_thresh, np.ones(N)/N, mode='valid')
-  #above_thresh = uniform_filter1d(above_thresh.astype(float), size=N)
-  #above_thresh = above_thresh > 0
-
-
-  # TODO: quick hack to remove oscillating
-  # strategy: fill short gaps
+  # remove oscillating by filling short gaps
   first_below_idx = -1000000
   for i in range(1, len(above_thresh)):
     if not above_thresh[i] and above_thresh[i - 1]:
       first_below_idx = i
     if above_thresh[i] and not above_thresh[i - 1]:
-      if i - first_below_idx < min_on_detection_gap * sample_rate:
+      if i - first_below_idx < min_strike_len * sample_rate:
         above_thresh[range(first_below_idx, i)] = True
+
   # remove very short on periods
   first_above_idx = -1000000
   for i in range(1, len(above_thresh)):
     if above_thresh[i] and not above_thresh[i - 1]:
       first_above_idx = i
     if not above_thresh[i] and above_thresh[i - 1]:
-      if i - first_above_idx < min_on_detection_gap * sample_rate:
+      if i - first_above_idx < min_strike_len * sample_rate:
         above_thresh[range(first_above_idx, i)] = False
-
-
-
-  ## TODO: quick hack to remove oscillating at the end of a detected block
-  ## strategy: on period at least min_pause_between_strikes long
-  #last_above_idx = -1000000
-  #for i in range(1, len(above_thresh)):
-  #  if above_thresh[i] and not above_thresh[i - 1]:
-  #    last_above_idx = i
-  #  if not above_thresh[i] and above_thresh[i - 1]:
-  #    if i - last_above_idx < min_pause_between_strikes * sample_rate:
-  #      above_thresh[i] = True
-
-
-
-  ### TODO: quick hack to remove oscillating at the end of a detected block
-  ### strategy: remove bursts after short gaps
-  ##last_above_idx = -1000000
-  ##for i in range(1, len(above_thresh)):
-  ##  if above_thresh[i] and not above_thresh[i - 1]:
-  ##    if i - last_above_idx < min_pause_between_strikes * sample_rate:
-  ##      above_thresh[i] = False
-  ##  if above_thresh[i]:
-  ##    last_above_idx = i
-
 
   strike_start = np.argwhere(np.diff(above_thresh.astype(float)) > 0)
   strike_end   = np.argwhere(np.diff(above_thresh.astype(float)) < 0)
-
-
 
   # extract individual samples from long sample vector
   sample_powers  = [[]] * len(strike_start)
@@ -238,28 +151,25 @@ for instrument in instruments:
   #plt.plot(sample_strikes[7][:, 0])
   #plt.show()
 
-  ##plt.plot(20 * np.log10(np.abs(sample_float[master_channel])))
-  #plt.plot(10 * np.log10(np.abs(x)))
-  #plt.plot([0, len(x)], 10 * np.log10([threshold, threshold]))
-  #plt.plot(10 * np.log10(np.max(x)) * above_thresh)
-  #plt.title(instrument_name)
-  #plt.show()
+  #plt.plot(20 * np.log10(np.abs(sample_float[master_channel])))
+  plt.plot(10 * np.log10(np.abs(x)))
+  plt.plot([0, len(x)], 10 * np.log10([threshold, threshold]))
+  plt.plot(10 * np.log10(np.max(x)) * above_thresh)
+  plt.title(instrument_name)
+  plt.show()
 
 
   ##############################################################################
-  # PREPARE INSTRUMENT XML FILE ################################################
+  # WRITE WAVE FORMS AND INSTRUMENT XML FILE ###################################
   ##############################################################################
   instrument_xml = ET.Element("instrument")
   instrument_xml.set("version", "2.0")
   instrument_xml.set("name", instrument_name)
   samples_xml = ET.SubElement(instrument_xml, "samples")
 
-
   for i in range(0, len(sample_strikes)):
 
-    ############################################################################
-    # CREATE WAVE FORMS ########################################################
-    ############################################################################
+    # file/path handling
     instrument_path        = kit_name + "/" + instrument_name + "/"
     instrument_sample_path = instrument_path + samples_dir_name + "/"
     sample_file_name       = str(i + 1) + "-" + instrument_name
@@ -278,10 +188,7 @@ for instrument in instruments:
       audiofile_xml.set("file", samples_dir_name + "/" + sample_file_name + ".wav")
       audiofile_xml.set("filechannel", str(j + 1))
 
-
-  ##############################################################################
-  # WRITE INSTRUMENT XML FILE ##################################################
-  ##############################################################################
+  # write instrument XML file
   tree_xml = ET.ElementTree(instrument_xml)
   ET.indent(instrument_xml, space="\t", level=0)
   tree_xml.write(instrument_path + instrument_name + ".xml", encoding="utf-8", xml_declaration="True")
