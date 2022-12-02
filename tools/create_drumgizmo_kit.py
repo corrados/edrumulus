89bca1b3f5bc5c2a9d4f361c29b8307b2fc48593
Volker Fischer <corrados@users.noreply.github.com>
2022-12-02 20:10:28 +0100

    backup checking of positional sensing support (not yet finished)

--- tools/create_drumgizmo_kit.py
+++ tools/create_drumgizmo_kit.py
@@ -25,16 +25,8 @@ import xml.etree.ElementTree as ET
 from scipy.io import wavfile
 
 
-
-
-# TODO positional sensing support
-
-
-
-
-
 ################################################################################
-# INITIALIZATIONS ##############################################################
+# CONFIGURATION AND INITIALIZATIONS ############################################
 ################################################################################
 # instruments: [instrument_name, master_channel, MIDI_note, threshold]
 instruments = [["kick",          "KDrum",   36, 45], \
@@ -50,7 +42,7 @@ instruments = [["kick",          "KDrum",   36, 45], \
                ["ride_bell",     "OHRight", 53, 60]]
 
 # TEST for optimizing the analization algorithms, only use one instrument
-#instruments = [instruments[0]]
+instruments = [instruments[0]]
 
 kit_name                = "PearlMMX" # avoid spaces
 samples_dir_name        = "samples" # compatible to other Drumgizmo kits
@@ -71,103 +63,97 @@ for instrument in instruments:
   instrument_path        = kit_name + "/" + instrument_name + "/"
   instrument_sample_path = instrument_path + samples_dir_name + "/"
   base_instrument_name   = instrument_name.split("_")[0]
-  position               = -1 # default: invalid position, i.e., no positional support
   print(instrument_name)
 
-  # TEST
-  file_names      = os.listdir(source_samples_dir_name + "/" + base_instrument_name)
-  file_name_parts = [[]] * len(file_names)
+  # check if instrument has positional sensing support and extract position indexes
+  file_names = os.listdir(source_samples_dir_name + "/" + base_instrument_name)
+  positions  = []
   for i, file_name in enumerate(file_names):
-    file_name_parts[i] = file_name.split(".")[0].split("_")
-    if len(file_name_parts[i]) > 2 and len(file_name_parts[i][-2]) == 1: # position information always second last item and one character long
+    file_name_parts = file_name.split(".")[0].split("_")
+    # position information always second last item and one character long
+    if len(file_name_parts) > 2 and len(file_name_parts[-2]) == 1:
+      positions.append(int(file_name_parts[-2]))
 
-      # TODO right now no position support
-      position = 0
+  positions = list(dict.fromkeys(positions)) # remove duplicates
+  positions = [-1] if not positions else positions # if no positions, use -1 (i.e. no positionsl support)
 
-      #position = int(file_name_parts[i][-2])
-    else:
-      pass
-      #position = -1 # invalid position value
+  sample_powers  = [[]] * len(positions)
+  sample_strikes = [[]] * len(positions)
 
-  # create file names of all audio channels
-  file_names = []
-  for i in range(0, num_channels):
-    pos_str = "_" + str(position) if position >= 0 else ""
-    file_names.append(source_samples_dir_name + "/" + base_instrument_name + "/" + \
-                      instrument_name + pos_str+ "_channel" + str(i + 1) + ".wav")
+  for p in positions:
 
-
-  ##############################################################################
-  # READ WAVE FORMS ############################################################
-  ##############################################################################
-  # read samples from all audio channels
-  sample = [[]] * num_channels
-  for i, f in enumerate(file_names):
-    with wave.open(f, "r") as file:
-      sample_rate = file.getframerate() # assuming all wave have the same rate
-      sample[i]   = np.frombuffer(file.readframes(-1), np.int16) # assuming 16 bit
+    ##############################################################################
+    # READ WAVE FORMS ############################################################
+    ##############################################################################
+    sample  = [[]] * num_channels
+    pos_str = "_" + str(p) if p >= 0 else ""
+    for i in range(0, num_channels):
+      with wave.open(source_samples_dir_name + "/" + base_instrument_name + "/" + \
+                     instrument_name + pos_str + "_channel" + str(i + 1) + ".wav", "r") as file:
+        sample_rate = file.getframerate() # assuming all wave have the same rate
+        sample[i]   = np.frombuffer(file.readframes(-1), np.int16) # assuming 16 bit
 
 
-  ##############################################################################
-  # WAVE FORM ANALYSIS #########################################################
-  ##############################################################################
-  thresh_from_max = instrument[3] # dB from maximum peak
-  master_channel  = channel_names.index(instrument[1])
+    ##############################################################################
+    # WAVE FORM ANALYSIS #########################################################
+    ##############################################################################
+    thresh_from_max = instrument[3] # dB from maximum peak
+    master_channel  = channel_names.index(instrument[1])
 
-  # find samples which are above the threshold
-  x            = np.square(sample[master_channel].astype(float))
-  threshold    = np.power(10, (10 * np.log10(np.max(x)) - thresh_from_max) / 10)
-  above_thresh = x > threshold
+    # find samples which are above the threshold
+    x            = np.square(sample[master_channel].astype(float))
+    threshold    = np.power(10, (10 * np.log10(np.max(x)) - thresh_from_max) / 10)
+    above_thresh = x > threshold
 
-  # remove oscillating by filling short gaps
-  first_below_idx = -100 * sample_rate
-  for i in range(1, len(above_thresh)):
-    if not above_thresh[i] and above_thresh[i - 1]:
-      first_below_idx = i
-    if above_thresh[i] and not above_thresh[i - 1]:
-      if i - first_below_idx < min_strike_len * sample_rate:
-        above_thresh[range(first_below_idx, i)] = True
+    # remove oscillating by filling short gaps
+    first_below_idx = -100 * sample_rate
+    for i in range(1, len(above_thresh)):
+      if not above_thresh[i] and above_thresh[i - 1]:
+        first_below_idx = i
+      if above_thresh[i] and not above_thresh[i - 1]:
+        if i - first_below_idx < min_strike_len * sample_rate:
+          above_thresh[range(first_below_idx, i)] = True
 
-  # remove very short on periods
-  first_above_idx = -100 * sample_rate
-  for i in range(1, len(above_thresh)):
-    if above_thresh[i] and not above_thresh[i - 1]:
-      first_above_idx = i
-    if not above_thresh[i] and above_thresh[i - 1]:
-      if i - first_above_idx < min_strike_len * sample_rate:
-        above_thresh[range(first_above_idx, i)] = False
+    # remove very short on periods
+    first_above_idx = -100 * sample_rate
+    for i in range(1, len(above_thresh)):
+      if above_thresh[i] and not above_thresh[i - 1]:
+        first_above_idx = i
+      if not above_thresh[i] and above_thresh[i - 1]:
+        if i - first_above_idx < min_strike_len * sample_rate:
+          above_thresh[range(first_above_idx, i)] = False
 
-  strike_start = np.argwhere(np.diff(above_thresh.astype(float)) > 0)
-  strike_end   = np.argwhere(np.diff(above_thresh.astype(float)) < 0)
+    strike_start = np.argwhere(np.diff(above_thresh.astype(float)) > 0)
+    strike_end   = np.argwhere(np.diff(above_thresh.astype(float)) < 0)
 
-  # extract individual samples from long sample vector and estimate strike power
-  sample_powers  = [[]] * len(strike_start)
-  sample_strikes = [[]] * len(strike_start)
-  for i, (start, end) in enumerate(zip(strike_start, strike_end)):
-    # estimate power from master channel using the maximum value
-    x_cur_strike_master = x[range(start[0], end[0])]
-    sample_powers[i]    = str(np.max(x_cur_strike_master) / 32768 / 32768) # assuming 16 bit
+    # extract individual samples from long sample vector and analyze/process
+    sample_powers[p]  = [[]] * len(strike_start)
+    sample_strikes[p] = [[]] * len(strike_start)
+    for i, (start, end) in enumerate(zip(strike_start, strike_end)):
+      # estimate power from master channel using the maximum value
+      x_cur_strike_master = x[range(start[0], end[0])]
+      sample_powers[p][i]    = str(np.max(x_cur_strike_master) / 32768 / 32768) # assuming 16 bit
 
-    # extract sample data of current strike
-    sample_strikes[i] = np.zeros((end[0] - start[0] + 1, num_channels), np.int16)
-    for c in range(0, num_channels):
-      sample_strikes[i][:, c] = sample[c][start[0]:end[0] + 1]
+      # extract sample data of current strike
+      sample_strikes[p][i] = np.zeros((end[0] - start[0] + 1, num_channels), np.int16)
+      for c in range(0, num_channels):
+        sample_strikes[p][i][:, c] = sample[c][start[0]:end[0] + 1]
 
-      # audio fade-out at the end
-      sample_len = len(sample_strikes[i][:, c])
-      fade_start = int(sample_len * (1 - fade_out_percent / 100))
-      fade_len   = sample_len - fade_start
-      sample_strikes[i][fade_start:, c] = np.int16(sample_strikes[i][fade_start:, c].astype(float) * np.arange(fade_len + 1, 1, -1) / fade_len)
+        # audio fade-out at the end
+        sample_len = len(sample_strikes[p][i][:, c])
+        fade_start = int(sample_len * (1 - fade_out_percent / 100))
+        fade_len   = sample_len - fade_start
+        sample_strikes[p][i][fade_start:, c] = np.int16(sample_strikes[p][i][fade_start:, c].astype(float) * np.arange(fade_len + 1, 1, -1) / fade_len)
 
-    #print(sample_powers[i])
-    #plt.plot(sample_strikes[i][:, master_channel])
+      #print(sample_powers[p][i])
+      #plt.plot(sample_strikes[p][i][:, master_channel])
+      #plt.show()
+
+    #plt.plot(10 * np.log10(np.abs(x)))
+    #plt.plot([0, len(x)], 10 * np.log10([threshold, threshold]))
+    #plt.plot(10 * np.log10(np.max(x)) * above_thresh)
+    #plt.title(instrument_name)
     #plt.show()
-
-  #plt.plot(10 * np.log10(np.abs(x)))
-  #plt.plot([0, len(x)], 10 * np.log10([threshold, threshold]))
-  #plt.plot(10 * np.log10(np.max(x)) * above_thresh)
-  #plt.title(instrument_name)
-  #plt.show()
 
 
   ##############################################################################
@@ -178,21 +164,22 @@ for instrument in instruments:
   instrument_xml.set("name", instrument_name)
   samples_xml = ET.SubElement(instrument_xml, "samples")
 
-  for i in range(0, len(sample_strikes)):
-    # write multi-channel wave file
-    sample_file_name = str(i + 1) + "-" + instrument_name
-    os.makedirs(instrument_sample_path, exist_ok=True)
-    wavfile.write(instrument_sample_path + sample_file_name + ".wav", sample_rate, sample_strikes[i])
+  for p in positions:
+    for i in range(0, len(sample_strikes[p])):
+      # write multi-channel wave file
+      sample_file_name = str(i + 1) + "-" + instrument_name
+      os.makedirs(instrument_sample_path, exist_ok=True)
+      wavfile.write(instrument_sample_path + sample_file_name + ".wav", sample_rate, sample_strikes[p][i])
 
-    # write XML content for current sample
-    sample_xml = ET.SubElement(samples_xml, "sample")
-    sample_xml.set("name", instrument_name + "-" + str(i + 1))
-    sample_xml.set("power", sample_powers[i])
-    for j, channel_name in enumerate(channel_names):
-      audiofile_xml = ET.SubElement(sample_xml, "audiofile")
-      audiofile_xml.set("channel", channel_name)
-      audiofile_xml.set("file", samples_dir_name + "/" + sample_file_name + ".wav")
-      audiofile_xml.set("filechannel", str(j + 1))
+      # write XML content for current sample
+      sample_xml = ET.SubElement(samples_xml, "sample")
+      sample_xml.set("name", instrument_name + "-" + str(i + 1))
+      sample_xml.set("power", sample_powers[p][i])
+      for j, channel_name in enumerate(channel_names):
+        audiofile_xml = ET.SubElement(sample_xml, "audiofile")
+        audiofile_xml.set("channel", channel_name)
+        audiofile_xml.set("file", samples_dir_name + "/" + sample_file_name + ".wav")
+        audiofile_xml.set("filechannel", str(j + 1))
 
   # write instrument XML file
   tree_xml = ET.ElementTree(instrument_xml)
