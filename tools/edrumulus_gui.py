7a620054438cc23119c20cba72836a958feb4b01
Volker Fischer <corrados@users.noreply.github.com>
2023-01-08 13:38:14 +0100

    better auto pad selection implementation

--- tools/edrumulus_gui.py
+++ tools/edrumulus_gui.py
@@ -27,11 +27,12 @@ import time
 # tables
 max_num_pads = 9
 midi_map     = {38: "snare", 40: "snare", 36: "kick", 22: "hi-hat", 26: "hi-hat", 44: "pedal", \
-                49: "crash", 51: "ride",  48: "tom1", 45: "tom2",   43: "tom3"}
+                49: "crash", 55: "crash", 51: "ride", 48: "tom1",   50: "tom1", \
+                45: "tom2",  47: "tom2",  43: "tom3", 58: "tom3"}
 curve_types  = ["LINEAR", "EXP1", "EXP2", "LOG1", "LOG2"]
 pad_names    = ["snare", "kick", "hi-hat", "ctrl", "crash", "tom1", "ride", "tom2", "tom3"]
-pad_types    = ["PD120", "PD80R", "PD8", "FD8", "VH12", "VH12CTRL", "KD7", "TP80", "CY6", "CY8", "DIABOLO12", \
-                "CY5", "HD1TOM", "PD6", "KD8", "PDX8", "KD120", "PD5", "PDA120LS", "PDX100", "KT10"]
+pad_types    = ["PD120", "PD80R",  "PD8", "FD8", "VH12", "VH12CTRL", "KD7", "TP80",     "CY6",    "CY8", "DIABOLO12", \
+                "CY5",   "HD1TOM", "PD6", "KD8", "PDX8", "KD120",    "PD5", "PDA120LS", "PDX100", "KT10"]
 cmd_names    = [ "type", "thresh", "sens", "pos thres", "pos sens", "rim thres", "curve", "spike", "rim/pos", "note", "note rim", "cross" ]
 cmd_val      = [    102,      103,    104,         105,        106,         107,     109,     110,       111,    112,        113,     114 ]
 cmd_val_rng  = [     20,       31,     31,          31,         31,          31,       4,       4,         3,    127,        127,      31 ]
@@ -108,15 +109,6 @@ def update_param_outputs():
   ctrlwin.refresh()
 
 
-# update pad selection (for auto pad selection)
-def update_pad_selection(midi_note_in, midi_note1, midi_note2, pad_index):
-  global sel_pad, midi_send_val, midi_send_cmd
-  if (midi_note_in == midi_note1 or midi_note_in == midi_note2) and sel_pad is not pad_index:
-    sel_pad       = pad_index
-    midi_send_val = sel_pad
-    midi_send_cmd = 108
-
-
 def store_settings():
   with open("settings/trigger_settings.txt", "w") as f:
     for (pad_index, pad) in enumerate(pad_names):
@@ -138,7 +130,7 @@ def load_settings():
       (pad, command, value) = line.replace('\n', '').split(',')
       if int(command) in cmd_val:
         if cur_pad != int(pad):
-          database = [0] * 128 # reset database
+          database = [0] * len(cmd_val) # reset database
           cur_pad  = int(pad)
           send_value_to_edrumulus(108, cur_pad)
         send_value_to_edrumulus(int(command), int(value))
@@ -159,7 +151,7 @@ def send_value_to_edrumulus(command, value):
 @client.set_process_callback
 def process(frames):
   global database, midi_send_val, midi_send_cmd, midi_previous_send_cmd, do_update_param_outputs, \
-         version_major, version_minor, hi_hat_ctrl
+         version_major, version_minor, hi_hat_ctrl, sel_pad
   output_port.clear_buffer()
   for offset, data in input_port.incoming_midi_events():
     if len(data) == 3:
@@ -192,15 +184,15 @@ def process(frames):
         midigwin.move(2, 1)
         midigwin.hline(curses.ACS_BLOCK, max(1, int(float(value) / 128 * 25)))
         do_update_param_outputs = True
-        if auto_pad_sel and value > 10: # auto pad selection
-          update_pad_selection(key, 38, 40, 0) # snare
-          update_pad_selection(key, 36, 36, 1) # kick
-          update_pad_selection(key, 22, 26, 2) # hi-hat
-          update_pad_selection(key, 49, 55, 4) # crash
-          update_pad_selection(key, 48, 50, 5) # tom1
-          update_pad_selection(key, 51, 53, 6) # ride
-          update_pad_selection(key, 45, 47, 7) # tom2
-          update_pad_selection(key, 43, 58, 8) # tom3
+        if auto_pad_sel and instrument_name and value > 10: # auto pad selection (velocity threshold of 10)
+          try:
+            pad_index = pad_names.index(instrument_name) # throws exception if pad was not found
+            if sel_pad is not pad_index: # only change pad if it is different from current
+              sel_pad       = pad_index
+              midi_send_val = sel_pad
+              midi_send_cmd = 108
+          except:
+            pass # pad not found, do nothing
 
       if (status & 0xF0) == 0xB0: # display current positional sensing received value
         if key == 16: # positional sensing
