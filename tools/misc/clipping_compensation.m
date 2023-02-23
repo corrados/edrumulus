cd5cb86da70f37cfa6a236c63831f9da1241769a
Volker Fischer <corrados@users.noreply.github.com>
2023-02-23 18:17:46 +0100

    show two pad results in one graph

--- tools/misc/clipping_compensation.m
+++ tools/misc/clipping_compensation.m
@@ -4,23 +4,33 @@
 pkg load signal
 close all;
 
-% load test data
-x = audioread("../../algorithm/signals/pd120_single_hits.wav");
+test_files = {"../../algorithm/signals/pd120_single_hits.wav", 9917:9931; ...
+              "../../algorithm/signals/pd8.wav",               67140:67146};
 
-% pick one peak
-x_org = x(9917:9931, :);
-
-clip_limit_range = 0.01:0.001:0.1;
+clip_limit_range = 0.05:0.001:1;
 num_clipped_val  = [];
 
-for idx = 1:length(clip_limit_range)
+for i = 1:size(test_files, 1)
 
-  % clip
-  clip_limit = clip_limit_range(idx);
-  x          = max(-clip_limit, min(clip_limit, x_org));
+  % load test data
+  x = audioread(test_files{i, 1});
 
-  % count clipped values
-  num_clipped_val(idx) = length(find(abs(x - clip_limit) < 5 / 2^12));
+  % pick one peak and normalize
+  x_org = x(test_files{i, 2}, :);
+  x_org = x_org * 1 / max(x_org);
+
+  for idx = 1:length(clip_limit_range)
+
+    % clip
+    clip_limit = clip_limit_range(idx);
+    x          = max(-clip_limit, min(clip_limit, x_org));
+
+    % count clipped values
+    num_clipped_val(idx, i) = length(find(abs(x - clip_limit) < 5 / 2^12));
+
+  end
+
+%figure; plot(num_clipped_val(:, i), 20 * log10(clip_limit_range)); grid on;
 
 end
 
