859e16f35fb44712e935fb2515773488841134c2
Volker Fischer <corrados@users.noreply.github.com>
2023-02-25 20:41:49 +0100

    some more peak analysis for clipping compensation

--- tools/misc/clipping_compensation.m
+++ tools/misc/clipping_compensation.m
@@ -4,33 +4,42 @@
 pkg load signal
 close all;
 
-test_files = {"../../algorithm/signals/pd120_single_hits.wav", 9917:9931; ...
-              "../../algorithm/signals/pd8.wav",               67140:67146};
+test_files = {"../../algorithm/signals/pd120_single_hits.wav", {9917:9931, 14974:14985, 22525:22538, 35014:35025}; ...
+              "../../algorithm/signals/pd8.wav",               {67140:67146, 73359:73363, 252036:252039, 296753:296757}};
 
 clip_limit_range = 0.05:0.001:1;
 num_clipped_val  = [];
+cnt              = 1;
 
 for i = 1:size(test_files, 1)
 
   % load test data
   x = audioread(test_files{i, 1});
 
-  % pick one peak and normalize
-  x_org = x(test_files{i, 2}, :);
-  x_org = x_org * 1 / max(x_org);
+%figure; plot(x)
 
-  for idx = 1:length(clip_limit_range)
+  for j = 1:length(test_files{i, 2})
 
-    % clip
-    clip_limit = clip_limit_range(idx);
-    x          = max(-clip_limit, min(clip_limit, x_org));
+    % pick one peak and normalize
+    x_org = x(test_files{i, 2}{j}, :);
+    x_org = x_org * 1 / max(x_org);
 
-    % count clipped values
-    num_clipped_val(idx, i) = length(find(abs(x - clip_limit) < 5 / 2^12));
+    for idx = 1:length(clip_limit_range)
 
-  end
+      % clip
+      clip_limit = clip_limit_range(idx);
+      y          = max(-clip_limit, min(clip_limit, x_org));
+
+      % count clipped values
+      num_clipped_val(idx, cnt) = length(find(abs(y - clip_limit) < 5 / 2^12));
+
+    end
+
+    cnt = cnt + 1;
 
 %figure; plot(num_clipped_val(:, i), 20 * log10(clip_limit_range)); grid on;
+
+  end
 
 end
 
