ae41c05b2346ad686486f7299b78e80dc534159e
Volker Fischer <corrados@users.noreply.github.com>
2023-04-02 13:15:36 +0200

    clean up unused code and remove log_correction

--- tools/misc/clipping_compensation.m
+++ tools/misc/clipping_compensation.m
@@ -7,36 +7,17 @@
 pkg load signal
 close all;
 
-testsignal         = 0;
-use_log_correction = false;
-%use_log_correction = true;
-
-%test_files = {"../../algorithm/signals/pd120_single_hits.wav", {9917:9931, 14974:14985, 22525:22538, 35014:35025}; ...
-%              "../../algorithm/signals/pd8.wav",               {67140:67146, 70170:70175, 73359:73363, 246312:246317, 252036:252039, 296753:296757}};
-%attenuation_mapping = -(0:20); % 1 dB per number of clipping samples -> current implementation in C++
+testsignal = 0;
 
 if testsignal == 0
-  test_files = {"../../algorithm/signals/pd120_single_hits.wav", {9917:9931, 14974:14985, 22525:22538, 35014:35025}};
 
-  if use_log_correction
-    attenuation_mapping = -[0, 0.22, 0.8, 1.3, 2, 3, 5, 6.5, 9, 12, 16, 23, 30, 40, 50]; % optimized for PD120
-  else
-    attenuation_mapping = 10 .^ (-[0:0.09:3] .^ 2);
-%attenuation_mapping = 10 .^ ((-[0:0.4:10] .^ 2) / 20);%-[0, 0.2, 1, 1.8, 2.7, 4.5, 5.5, 9, 10, 15, 18, 20, 30, 40];%[0, 0.22, 0.8, 1.3, 2, 3, 5, 6.5, 9, 12, 16, 23, 30, 40, 50]; % optimized for PD120
-  end
+  test_files = {"../../algorithm/signals/pd120_single_hits.wav", {9917:9931, 14974:14985, 22525:22538, 35014:35025}};
+  attenuation_mapping = 10 .^ (-[0:0.09:3] .^ 2);
 
 else
+
   test_files = {"../../algorithm/signals/pd8.wav", {67140:67146, 70170:70175, 73359:73363, 246312:246317, 252036:252039, 296753:296757}};
-
-  if use_log_correction
-    attenuation_mapping = -[0, 6.5, 10, 17:40];%[0, 9, 39, 13:40]; % optimized for PD8
-  else
-
-% TEST
-attenuation_mapping = 10 .^ (-[0:0.56:5] .^ 2);
-%attenuation_mapping = 10 .^ ((-[0, 6, 11, 30, 50:100]) / 20);%[0, 9, 39, 13:40]; % optimized for PD8
-
-  end
+  attenuation_mapping = 10 .^ (-[0:0.56:5] .^ 2);
 
 end
 
@@ -86,56 +67,18 @@ if num_clipped_val(idx, cnt) > 0
 
   if (left_index > 0) && (right_index <= length(x_org))
 
-    max_offset = attenuation_mapping(1 + num_clipped_val(idx, cnt) - 1) - attenuation_mapping(1 + num_clipped_val(idx, cnt));
 
-    neighbor = max(0, mean([x_org(left_index), x_org(right_index)]));
+    neighbor = mean([x_org(left_index), x_org(right_index)]);
 
-    %neighbor = max(0, max(x_org(left_index), x_org(right_index)));
+    % TEST: use linear domain for offset calculation
+    offset = clip_limit - neighbor;
 
-%%20 * log10(abs(x_org(left_index) / x_org(right_index)))
-%if abs(20 * log10(abs(x_org(left_index) / x_org(right_index)))) > 20
-%  neighbor = max(0, max(x_org(left_index), x_org(right_index)));
-%else
-%  neighbor = max(0, mean([x_org(left_index), x_org(right_index)]));
-%end
+    %max_offset = attenuation_mapping(1 + num_clipped_val(idx, cnt) - 1) - attenuation_mapping(1 + num_clipped_val(idx, cnt));
+    %offset = min(offset, 10 ^ (max_offset / 20));
 
-    %neighbor = max(0, sqrt(mean([x_org(left_index) ^ 2, x_org(right_index) ^ 2])));
-    %neighbor = max(0, mean([sqrt(x_org(left_index)), sqrt(x_org(right_index))]) ^ 2);
+    attenuation_compensation(idx, cnt) = 20 * log10(attenuation_compensation(idx, cnt) + offset);
 
-    %neighbor = max(0, mean([min(x_org(left_index), max_offset), min(x_org(right_index), max_offset)]));
-    %neighbor = max(0, min(x_org(left_index), x_org(right_index)));
 
-%disp([num2str(clip_limit) ', ' num2str(neighbor)])
-
-    if use_log_correction
-
-      offset = 20 * log10(clip_limit / neighbor);
-      offset = min(offset, max_offset);
-
-%% if mean of neighors is way off, try out max instead
-%if offset == max_offset
-%  neighbor = max(0, max(x_org(left_index), x_org(right_index)));
-%  offset   = 20 * log10(clip_limit / neighbor);
-%  offset   = min(offset, max_offset);
-%end
-
-%disp([num2str(attenuation_compensation(idx, cnt)) ', ' num2str(offset)])
-
-      attenuation_compensation(idx, cnt) = attenuation_compensation(idx, cnt) + offset;
-
-    else
-
-% TEST
-neighbor = mean([x_org(left_index), x_org(right_index)]);
-%neighbor = max(x_org(left_index), x_org(right_index));
-
-      % TEST: use linear domain for offset calculation
-      offset = clip_limit - neighbor;
-      %offset = min(offset, 10 ^ (max_offset / 20));
-
-      attenuation_compensation(idx, cnt) = 20 * log10(attenuation_compensation(idx, cnt) + offset);
-
-    end
 
     correction_offset_applied = true;
 
@@ -145,11 +88,7 @@ end
 if ~correction_offset_applied
 
   % if no neighbors are available, use worst case assumption of last attenuation
-  if use_log_correction
-    attenuation_compensation(idx, cnt) = attenuation_mapping(1 + num_clipped_val(idx, cnt) - 1);
-  else
-    attenuation_compensation(idx, cnt) = 20 * log10(attenuation_mapping(1 + num_clipped_val(idx, cnt) - 1));
-  end
+  attenuation_compensation(idx, cnt) = 20 * log10(attenuation_mapping(1 + num_clipped_val(idx, cnt) - 1));
 
 end
 
