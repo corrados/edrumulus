43fee6396ca262b6b6c780b2eea9ba459b639b50
Volker Fischer <corrados@users.noreply.github.com>
2024-01-07 18:01:13 +0100

    2023 -> 2024

--- algorithm/drumtrigger.m
+++ algorithm/drumtrigger.m
@@ -1,5 +1,5 @@
 %*******************************************************************************
-% Copyright (c) 2020-2023
+% Copyright (c) 2020-2024
 % Author(s): Volker Fischer
 %*******************************************************************************
 % This program is free software; you can redistribute it and/or modify it under
