075aa1ab109aff0edf3f743e36a23e7ac470689f
Volker Fischer <corrados@users.noreply.github.com>
2025-12-07 09:51:31 +0100

    try to fix firmware upload on Raspberry Pi 4 prototype 5

--- tools/flash_esp32.sh
+++ tools/flash_esp32.sh
@@ -45,7 +45,9 @@ pigs w 9 1
 #esptool.py -p /dev/serial0 erase_flash
 
 
-esptool.py --chip esp32 --port /dev/serial0 write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 flash_esp32_bin/boot_app0.bin 0x1000 flash_esp32_bin/bootloader_dio_80m.bin 0x10000 flash_esp32_bin/edrumulus.ino.bin 0x8000 flash_esp32_bin/edrumulus.ino.partitions.bin
+# taken from Arduino IDE v2.3.6 with esp32 v3.3.4 command window after pressing Upload button
+# path to boot file: /home/[user]/.arduino15/packages/esp32/hardware/esp32/3.3.4/tools/partitions/boot_app0.bin
+esptool.py --chip esp32 --port /dev/serial0 write_flash write-flash -z --flash-mode keep --flash-freq keep --flash-size keep 0x1000 flash_esp32_bin/edrumulus.ino.bootloader.bin 0x8000 flash_esp32_bin/edrumulus.ino.partitions.bin 0xe000 flash_esp32_bin/boot_app0.bin 0x10000 flash_esp32_bin/edrumulus.ino.bin
 
 
 # set IO0 to non-flash mode and reboot ESP32 -----------------------------------
---
+++ tools/flash_esp32_bin/readme.txt
@@ -0,0 +1,2 @@
+In Arduino IDE export binaries and copy them in this folder. Note that for current Arduino IDE, the binaries are stored in .cache directory.
+Path to boot file for esp32 v3.3.4 library: /home/[user]/.arduino15/packages/esp32/hardware/esp32/3.3.4/tools/partitions/boot_app0.bin
