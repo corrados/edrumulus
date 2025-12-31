f03464dae67894816f270acabc6d0c21b435149c
Volker Fischer <corrados@users.noreply.github.com>
2025-12-31 12:14:47 +0100

    bug fix

--- edrumulus.h
+++ edrumulus.h
@@ -101,7 +101,7 @@ class Edrumulus
   void set_spike_cancel_level(const int new_level) { spike_cancel_level = new_level; }
   int  get_spike_cancel_level() { return spike_cancel_level; }
 
-  void set_enable_load_indicator(const int new_status) { load_indicator = new_status != 0; }
+  void set_enable_load_indicator(const int new_enabled) { use_load_indicator = new_enabled != 0; }
 
   // error and overload handling (implement blinking LED for error using error_LED_blink_time)
   bool get_status_is_error() { return status_is_error && ((error_LED_cnt % error_LED_blink_time) < (error_LED_blink_time / 2)); }
